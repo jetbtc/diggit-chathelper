c84db6d7cb66be965420e72cfc88c3c6dcc1608a
jet <jetbtc@outlook.com>
2014-12-13 11:49:15 +0100

    Update chatHandler function to latest diggit source

--- src/chathelper.js
+++ src/chathelper.js
@@ -8,7 +8,7 @@
 
 var jetstuff = jetstuff || {};
 (function() {
-    var styles = '{{styles}}';
+    var style = $('<style>').append('{{styles}}').appendTo(document.head);
 
     function ChatHelper() {
         this.init();
@@ -60,7 +60,7 @@ var jetstuff = jetstuff || {};
                 users[id] = {
                     ignored: false,
                     names: name ? [name] : []
-                }
+                };
                 this.saveUserlist();
             } else if(name) {
                 lastIndex = users[id].names.length - 1;
@@ -89,15 +89,18 @@ var jetstuff = jetstuff || {};
             var id = data["userid"],
                 name = data["username"],
                 msg = data["msg"],
-                altNames = "";
+                altNames = "",
                 ignored = false,
-                messageHead = "";
-                messageBody = "";
+                idString;
 
             if(id) {
                 this.trackUser(id, name);
                 altNames = this.userlist[id].names.length > 1 ? "Previous names: " + this.userlist[id].names.slice(0,-1).join(', ') : "";
             }
+
+            idString = altNames
+                        ? '<span class="jetstuff-userid jetstuff-hasalts" title="'+altNames+'">'+(id || "")+'</span>'
+                        : '<span class="jetstuff-userid">'+(id || "")+'</span>';
 
             if (!msg) {
                 return;
@@ -108,61 +111,63 @@ var jetstuff = jetstuff || {};
                 ignored = true;
             }
 
-            var date = convertToLocalTime(new Date(data["date"]));
-            var hour = date.getHours();
-            var minute = date.getMinutes();
-            var doScroll = true;
-            if ($("#chatbox").scrollTop() + $("#chatbox").innerHeight() + 100 < $("#chatbox").get(0).scrollHeight) {
+            msg = msg.replace(/["']/g, "");
+            msg = msg.replace(/'/g, '');
+
+            var date = convertToLocalTime(new Date(data["date"])),
+                hour = date.getHours(),
+                minute = date.getMinutes(),
+                doScroll = true;
+
+            if($("#chatbox").scrollTop() + $("#chatbox").innerHeight() + 100 < $("#chatbox").get(0).scrollHeight) {
                 doScroll = false;
             }
             msg = msg.split(" ");
-            for (var i = 0; i < msg.length; i++) {
+
+            for(var i = 0; i < msg.length; i++) {
+                msg[i] = msg[i].replace(/["']/g, '');
                 var firsttwo = msg[i].substring(0, 2);
-                if (firsttwo == "G:" || firsttwo == "g:" || firsttwo == "B:" || firsttwo == "b:") {
+                if(firsttwo == "G:" || firsttwo == "g:" || firsttwo == "B:" || firsttwo == "b:") {
                     var split = msg[i].split(":");
-                    if (split.length > 0 && !isNaN(split[1])) {
-                        if (split[1].length <= 0 && split[1].length > 11 || split[1] < 0) {} else {
+                    if(split.length > 0 && !isNaN(split[1])) {
+                        if(split[1].length <= 0 && split[1].length > 11 || split[1] < 0) {} else {
                             msg[i] = '<span style="color:lightgreen" class="gameid" data-gameid="' + split[1] + '">G:' + split[1] + '</span>';
                         }
                     }
-                } else if (firsttwo == "U:" || firsttwo == "u:") {
+                } else if(firsttwo == "U:" || firsttwo == "u:") {
                     var split = msg[i].split(":");
-                    if (split.length > 0) {
-                        if (split[1].length <= 0 && split[1].length > 16 || split[1] < 0) {} else {
-                            if (isNaN(split[1])) {
+                    if(split.length > 0) {
+                        if(split[1].length <= 0 || split[1].length > 12 || split[1] < 0) {} else {
+                            if(isNaN(split[1])) {
                                 msg[i] = '<span style="color:lightgreen" class="puser" data-username="' + split[1] + '">U:' + split[1] + '</span>';
                             } else {
                                 msg[i] = '<span style="color:lightgreen" class="puser" data-userid="' + split[1] + '">U:' + split[1] + '</span>';
                             }
                         }
                     }
-                } else if (msg[i].length >= 26 && msg[i].length <= 34 && (msg[i].substring(0, 1) == "1" || msg[i].substring(0, 1) == "3")) {
+                } else if(msg[i].length >= 26 && msg[i].length <= 34 && (msg[i].substring(0, 1) == "1" || msg[i].substring(0, 1) == "3")) {
                     msg[i] = '<span style="color:lightgreen" class="hoverlink" onclick="window.open(\'https://blockchain.info/address/' + msg[i] + '\', \'_blank\');">' + msg[i] + '</span>';
-                } else if (!id && msg[i] == "%BTC") {
+                } else if(!id && msg[i] == "%BTC") {
                     msg[i] = tobtc(data["amount"]);
-                } else if (isURL(msg[i])) {
-                    msg[i] = '<span style="color:lightgreen" class="hoverlink" onclick="openURL(\'' + msg[i] + '\');">' + msg[i] + '</span>';
+                } else if(isURL(msg[i]) && msg[i].indexOf("'") === -1) {
+                    lastUrlID++;
+                    msg[i] = encodeURI(msg[i]);
+                    msg[i] = '<span style="color:lightgreen" class="hoverlink" id="url' + lastUrlID + '" onclick="openURL(' + lastUrlID + ');">' + msg[i] + '</span>';
                 }
             }
             msg = msg.join(" ");
-
-            messageHead = '<div class="chatuser"><span class="chatusertext ' + ((id) ? 'updateableusername puser' : '') + '" '+(ignored ? 'style="color:#b57a5a"' : '')+' data-userid="' + id + '">' + name + '</span> ' + (data["admin"] ? ' <span class="chatuseradmin">(staff)</span>' : "") + '<span class="activeText" data-userid="' + id + '"></span>' + '<span class="jetstuff-userid" style="font-size: 11px; margin-left: 4px; opacity: 0.5;cursor:'+(altNames ? 'pointer' : 'default')+';" title="'+altNames+'">'+(id || "")+'</span>' + '</div><div class="chattime">' + ("0" + hour).slice(-2) + ':' + ("0" + minute).slice(-2) + '</div>';
-            
-            if(ignored) {
-                messageBody = '<div style="clear:both"></div>';
-            } else {
-                messageBody = '<div class="chatmsg ' + (data["userid"] == myuser.getID() ? "chatmsgme" : "") + (!data["userid"] ? "chatmsgbot" : "") + (data["admin"] ? " chatmsgadmin" : "") + '">' + msg + '</div>';
-            }
-            
-
-            $("#chatbox").append('<div class="chatmsgcontainer'+  (ignored ? ' jetstuff-ignoredUser' : '') +'">' + messageHead + messageBody + '</div>');
+            $("#chatbox").append('' + '<div class="chatmsgcontainer '+(ignored ? 'jetstuff-ignoreduser' : '')+'">' + '    <div class="chatuser"><span class="chatusertext ' + ((id) ? 'updateableusername puser' : '') + '" data-userid="' + id + '">' + name + '</span> ' + (data["admin"] ? ' <span class="chatuseradmin">(staff)</span>' : "") + '<span class="activeText" data-userid="' + id + '"></span>' + '</div>' + idString + '    <div class="chattime">' + ("0" + hour).slice(-2) + ':' + ("0" + minute).slice(-2) + '</div>' + '    <div class="chatmsg ' + (data["userid"] == myuser.getID() ? "chatmsgme" : "") + (!data["userid"] ? "chatmsgbot" : "") + (data["admin"] ? " chatmsgadmin" : "") + '">' + msg + '</div>' + '</div>');
             $("#chatbox").stop();
-            if (!document.hasFocus()) {
+            if(!document.hasFocus()) {
                 chatmsgsblur++;
-                document.title = '(' + chatmsgsblur + ') ' + title;
+                var chatmsgstring = chatmsgsblur;
+                if(chatmsgsblur > 99) {
+                    chatmsgstring = "99+";
+                }
+                document.title = '(' + chatmsgstring + ') ' + title;
             }
             update_chatPreview();
-            if (doScroll) {
+            if(doScroll) {
                 $("#chatbox").animate({
                     "scrollTop": $('#chatbox')[0].scrollHeight
                 }, "slow");
@@ -180,7 +185,7 @@ var jetstuff = jetstuff || {};
                     this.userlist[id] = {
                         names: [],
                         ignored: true
-                    }
+                    };
                 }
                 this.saveUserlist();
                 localStorage.removeItem('jetstuff.chathelper.ignoredusers');
